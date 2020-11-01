143ab4b27cd5fd5bf9bf144e7948e05e2e4b5f5a
Melbage <tubby@Melbage.co.uk>
2020-11-01 13:48:35 +0000

    adding attemp at js code to read playersc json

--- Code/Python/ReadJson.py
+++ Code/Python/ReadJson.py
@@ -8,9 +8,16 @@ with open('./Code/JSON/Data/ExampleSCv2.json') as Schema:
 #print(PlayerScoreCard['Properties'])
 #print(PlayerScoreCard['Event'])
 #print(PlayerScoreCard['ScoreCard']['Holes'])
-
+TSI =0
 for enum,Hole in enumerate(PlayerScoreCard['ScoreCard']['Holes']):
     if enum ==0 : #this should be the totals
         print(Hole)
-        Hole['Ferrits'] = '18'
+        print(type(Hole))
+        Hole['Ferrits'] = 18
         print(Hole)
+    TSI= TSI+Hole['StokeIndex']
+#print(TSI)
+
+print(PlayerScoreCard['ScoreCard']['Holes'][0]['StokeIndex'])
+PlayerScoreCard['ScoreCard']['Holes'][0]['StokeIndex'] = TSI
+print(PlayerScoreCard['ScoreCard']['Holes'][0]['StokeIndex'])
