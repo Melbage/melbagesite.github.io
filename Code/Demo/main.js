394d3a0759c5305d9ffa8cb2770c729e124621bf
Melbage <tubby@Melbage.co.uk>
2020-11-07 17:03:47 +0000

    testing

--- Code/Demo/main.js
+++ Code/Demo/main.js
@@ -1,6 +1,10 @@
 var ourRequest = new XMLHttpRequest();
+//ourRequest.open('GET','https://melbage.github.io/melbagesite.github.io//Code/JSON/Data/ExampleSc.json');
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function(){
     console.log(ourRequest.responseText);
 };
 ourRequest.send();
+
+
+//ourRequest.open('GET','https://melbage.github.io/melbagesite.github.io//Code/JSON/Data/ExampleSc.json');
--- Code/Demo/mainFin.js
+++ Code/Demo/mainFin.js
@@ -4,7 +4,7 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+  ourRequest.open('GET', 'https://melbage.github.io/melbagesite.github.io//Code/JSON/Data/ExampleSc.json');
   ourRequest.onload = function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
