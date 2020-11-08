b05e1613b6575e66623aa772bc0da56f0e4380c7
Melbage <tubby@Melbage.co.uk>
2020-11-08 20:04:55 +0000

    sorting files.

--- Code/Demo/main.js
+++ Code/Demo/main.js
@@ -1,8 +1,9 @@
 var ourRequest = new XMLHttpRequest();
-//ourRequest.open('GET','https://melbage.github.io/melbagesite.github.io//Code/JSON/Data/ExampleSc.json');
-ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.open('GET','https://melbage.github.io/melbagesite.github.io//Code/JSON/Data/ExampleSc.json');
+//ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function(){
-    console.log(ourRequest.responseText);
+    var ourDate=JSON.parse(ourRequest.responseText);
+    console.log(ourDate.Player.PlayerName,ourDate.Player.BeforeHandicap);
 };
 ourRequest.send();
 
--- Code/JavaScript/readPlayerScoreCard.js
+++ Code/JavaScript/readPlayerScoreCard.js
@@ -1,24 +1,32 @@
-const petsData = [
-    {
-      name: "Purrsloud",
-      species: "Cat",
-      favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-      birthYear: 2017,
-      photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
-    },
-    {
-      name: "Barksalot",
-      species: "Dog",
-      birthYear: 2008,
-      photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-    },
-    {
-      name: "Meowsalot",
-      species: "Cat",
-      favFoods: ["tuna", "catnip", "celery"],
-      birthYear: 2012,
-      photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
-    }
-  ];
-  
-document.getElementById("PlayersScoreCard").innerHTML='Hello World'
+//reads in the HTLM tag for the scorecard and assign it to a variable
+var ScoreCardTable = document.getElementById("ScoreCard");
+
+//Read the JSON format ScoreCard from files system
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://melbage.github.io/melbagesite.github.io/Code/JSON/Data/ExampleSc.json');
+ourRequest.onload = function(){
+    //Read the file in and assin it to a JSON format
+    var ourData=JSON.parse(ourRequest.responseText);
+    //User a function to create the HTML code to display the table
+    renderHTML(ourData);
+    //console.log(ourDate.Player.PlayerName,ourDate.Player.BeforeHandicap);
+};
+ourRequest.send();
+
+function renderHTML(SCData){
+  var ScoreCardTable = document.getElementById("ScoreCard");
+  //set a HTMLString as the returned data
+  console.log(SCData.Player.PlayerName);
+  var HTMLString = "";
+  //HTMLString += "<p>"+ SCData.Player.PlayerName + "</p>";
+  //place the name at the top of the page.
+  HTMLString += '<tbody>';
+  HTMLString += '<tr height=25 style="height:18.75pt;width:50%;text-align:center">';
+  HTMLString += '<td>'+ SCData.Player.PlayerName + '</td></tr>';
+  HTMLString += '</tbody>';
+  //write the data to HMTL page
+  //ScoreCardTable.insertAdjacentHTML('beforeend', HTMLString);
+  console.log(HTMLString);
+  ScoreCardTable.insertAdjacentHTML('beforeend',HTMLString);
+
+};
